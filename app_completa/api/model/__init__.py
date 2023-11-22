420dde40105efa06cdbdcf1ab63e8db394c2f7c4
lumpleme <108637505+lumpleme@users.noreply.github.com>
2023-11-21 22:24:58 -0300

    Update __init__.py add tracker banco

--- app_completa/api/model/__init__.py
+++ app_completa/api/model/__init__.py
@@ -6,6 +6,7 @@ import os
 # importando os elementos definidos no modelo
 from model.base import Base
 from model.usuario import Usuario
+from model.tracker import Tracker
 
 db_path = "database/"
 # Verifica se o diretorio não existe
