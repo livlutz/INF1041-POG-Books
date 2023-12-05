78d126948db046e2de5c31cd25394b70172a0e96
DoctorTangerina <arthursardellajiujitsu@gmail.com>
2023-12-05 18:07:07 -0300

    Verificado o login para todas as operacoes

--- app_completa/api/app.py
+++ app_completa/api/app.py
@@ -6,11 +6,12 @@ from os import urandom
 from flask_openapi3 import Info, Tag
 from flask_openapi3 import OpenAPI
 from flask_cors import CORS
-from flask import request, redirect, render_template, flash
+from flask import request, redirect, render_template, flash, session
 from model import Usuario, Tracker, Lista, Livro, Session
 from logger import logger
 from schemas import *
 
+from functools import wraps
 
 info = Info(title="Minha querida API", version="1.0.0")
 app = OpenAPI(__name__, info=info)
@@ -20,7 +21,18 @@ CORS(app)
 # default
 @app.route('/')
 def home():
-    return redirect('/openapi')
+    return redirect('/home')
+
+# wrapper de verificar login
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect('/login')
+    return wrap
 
 # @app.route('/estante')
 # def estante():
@@ -35,6 +47,10 @@ def login():
         if request.form['submit'] == 'Cadastrar':
             return redirect('/sign_up')
         if validateLogin(request.form['username_cadastro'], request.form['senha_cadastro']):
+            session['logged_in'] = True
+            if 'email' not in session:
+                session['email'] = []
+            session['email'].append(request.form['username_cadastro'])
             return redirect('/')
         else:
             error = 'Email ou senha inválidos'
@@ -62,10 +78,18 @@ def sign_up():
             return redirect('/')
     return render_template('cadastro.html', error=error)
 
+# deslogar
+@app.route('/logout/')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    session.pop('email', None)
+    return redirect('/login')
+
 # valida login
 def validateLogin(email, senha):
-    session = Session()
-    query = session.query(Usuario).filter(Usuario.email == email)
+    sessionBD = Session()
+    query = sessionBD.query(Usuario).filter(Usuario.email == email)
     result = query.first()
     if result:
         if result.senha == senha:
@@ -74,26 +98,31 @@ def validateLogin(email, senha):
 
 # abre uma lista
 @app.route('/<lista_id>', methods=['GET'])
+@login_required
 def get_books(lista_id):
-    session = Session()
-    lista = session.query(Lista).filter(Lista.nome == lista_id).first()
+    sessionBD = Session()
+    user = sessionBD.query(Usuario).filter(Usuario.email == session['email']).first()
+    lista = sessionBD.query(Lista).filter(Lista.nome == lista_id and Lista.email == user.email).first()
     if not lista:
         error_msg = "Lista não encontrado na base :/"
         return render_template('error.html', error_code= 404, error_msg=error_msg), 404
     else:
-        livros = session.query(Livro).filter(Livro.nome == lista_id)
+        livros = sessionBD.query(Livro).filter(Livro.lista == lista.nome)
         return render_template('lista.html', livros = livros, lista_id = lista_id)
 
 # abre a tela de registrar livro
 @app.route('/<lista_id>/new_book', methods=['GET'])
+@login_required
 def register_book():
     return render_template('cadastro_livro.html'), 200
 
 # adiciona o livro na lista
 @app.route('/<lista_id>/new_book/save', methods=['POST'])
+@login_required
 def save_book(lista_id):
-    session = Session()
-    lista = session.query(Lista).filter(Lista.nome == lista_id).first()
+    sessionBD = Session()
+    user = sessionBD.query(Usuario).filter(Usuario.email == session['email']).first()
+    lista = sessionBD.query(Lista).filter(Lista.nome == lista_id and Lista.email == user.email).first()
     if not lista:
         error_msg = "Lista não encontrado na base :/"
         return render_template('error.html', error_code= 404, error_msg=error_msg), 404
@@ -123,8 +152,8 @@ def save_book(lista_id):
                 request.form['anotacao']
             )
             try:
-                session.add(livro)
-                session.commit()
+                sessionBD.add(livro)
+                sessionBD.commit()
                 return redirect('/<lista_id>', livro=livro), 200
             except IntegrityError as e:
                 error_msg = "Produto de mesmo nome já salvo na base :/"
@@ -139,10 +168,12 @@ def save_book(lista_id):
         
 # visualiza livro
 @app.route('/<lista_id>/<livro_id>', methods=['GET'])
+@login_required
 def get_livro(lista_id, livro_id):
-    session = Session()
-    lista = session.query(Lista).filter(Lista.nome == lista_id).first()
-    livro = session.query(Livro).filter(Livro.lista == lista_id and Livro.id == livro_id).first()
+    sessionBD = Session()
+    user = sessionBD.query(Usuario).filter(Usuario.email == session['email']).first()
+    lista = sessionBD.query(Lista).filter(Lista.nome == lista_id and Lista.email == user.email).first()
+    livro = sessionBD.query(Livro).filter(Livro.lista == lista.nome and Livro.id == livro_id).first()
     if not lista:
         error_msg = "Lista não encontrado na base :/"
         return render_template('error.html', error_code= 404, error_msg=error_msg), 404
@@ -151,10 +182,13 @@ def get_livro(lista_id, livro_id):
 
 # atualiza livro
 @app.route('/<lista_id>/<livro_id>/update', methods=['PUT'])
-def update_livro(livro_id):
-    session = Session()
+@login_required
+def update_livro(lista_id, livro_id):
+    sessionBD = Session()
     # Recupere o produto existente pelo ID
-    livro = session.query(Livro).get(livro_id)
+    user = sessionBD.query(Usuario).filter(Usuario.email == session['email']).first()
+    lista = sessionBD.query(Lista).filter(Lista.nome == lista_id and Lista.email == user.email).first()
+    livro = sessionBD.query(Livro).filter(Livro.id == livro_id and Livro.lista == lista.nome).first()
     if livro is None:
         return render_template("error.html", error_code=404, error_msg="Produto não encontrado :/"), 404
     # Atualize os campos do produto com os dados fornecidos no JSON da solicitação
@@ -180,7 +214,7 @@ def update_livro(livro_id):
     livro.anotacao = request.form['anotacao']
     try:
         # Efetue a atualização do produto
-        session.commit()
+        sessionBD.commit()
         return redirect("/<lista_id>/<livro_id>"), 200
     except IntegrityError as e:
         error_msg = "Erro na atualização do produto :/"
@@ -192,11 +226,14 @@ def update_livro(livro_id):
 
 # deletar livro
 @app.route('/<lista_id>/<livro_id>/delete', methods=['DELETE'])
+@login_required
 def delete_livro(lista_id, livro_id):
-    session = Session()
-    livro = session.query(Livro).filter(Livro.lista == lista_id and Livro.id == livro_id).first()
+    sessionBD = Session()
+    user = sessionBD.query(Usuario).filter(Usuario.email == session['email']).first()
+    lista = sessionBD.query(Lista).filter(Lista.nome == lista_id and Lista.email == user.email).first()
+    livro = sessionBD.query(Livro).filter(Livro.lista == lista.nome and Livro.id == livro_id).first()
     count = livro.delete()
-    session.commit()
+    sessionBD.commit()
     if count == 1:
         return redirect('/<lista_id>'), 200
     else: 
